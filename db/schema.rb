f4d226dadba214972f5d9322a5728be5e6d53d00
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-01 18:01:22 +0100

    feat(users table): add uniqueness at the database level on email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501141112) do
+ActiveRecord::Schema.define(version: 20160501165759) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20160501141112) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
