6564db90888ac8d2bb7e881c160c36eca9c6cec9
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-01 16:09:33 +0100

    test(user): ensure tests are passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\.]+[\w+]\.[a-z]+\z/i
-
-  validates :name, presence: true
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {minimum: 2}
   validates :password, presence: true, length: {minimum: 7}
   validates :email, presence: true, format: {with: VALID_EMAIL}, uniqueness: {case_sensitive: true}
   has_secure_password
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "User", type: :model do
     subject{ build(:user)}
 
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:name).is_at_least(2) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_length_of(:password).is_at_least(7) }
@@ -12,8 +12,19 @@ RSpec.describe "User", type: :model do
 
     describe "create User" do
     context "with valid User attributes" do
-      
+
       it{ is_expected.to be_valid}
+    end
+
+    context "with invalid User" do
+      it "email " do
+         subject.email = "@lekan"
+         expect(subject).to be_invalid
+      end
+      it "password and password_confirmation(not the same) " do
+         subject.password_confirmation = "test"
+         expect(subject).to be_invalid
+      end
     end
   end
 end
