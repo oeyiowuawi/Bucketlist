eb42cb5720c2e2d5c675a69045a2f1081ec8d837
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-01 13:58:13 +0100

    chore(rails helper): add configurations for database cleaner and factory girl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,8 +33,21 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
