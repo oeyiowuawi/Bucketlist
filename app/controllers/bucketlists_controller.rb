78477749e38f05c2a369675b4fdcc10cdedbdd03
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-02 19:47:17 +0100

    feat(update bucketlist): add update action to bucketlist controller

--- app/controllers/bucketlists_controller.rb
+++ app/controllers/bucketlists_controller.rb
@@ -28,6 +28,20 @@ class BucketlistsController < ApplicationController
       render json: {message: "bucketlist not found"}, status: 404
     end
   end
+
+  def update
+    bucketlist = current_user.bucket_lists.find_by(id: bucketlist_params[:id])
+    unless bucketlist.nil?
+      if bucketlist.update_attributes(name: bucketlist_params[:name])
+        render json: bucketlist, status: 200
+      else
+        render json: {errors: bucketlist.errors}, status: 422
+      end
+    else
+      render json: {errors: "can't update an invalid bucketlist"}, status: 404
+    end
+  end
+
   private
   def bucketlist_params
     params.permit(:name, :id, :page, :limit, :q)
--- spec/controllers/update_bucketlist_spec.rb
+++ spec/controllers/update_bucketlist_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'when trying to update a bucketlist',type: :request do
     @user1 = create(:user)
     @user2 = create(:user)
     @bucketlist1 = create(:bucket_list, created_by: @user1.id)
-    @bucketlist2, @bucketlist3 = create_list(:bucket_list, 2, created_by: @user2.id)
+    @bucketlist2 = create(:bucket_list, created_by: @user2.id)
   end
 
   context 'as a logged in user' do
@@ -30,14 +30,15 @@ RSpec.describe 'when trying to update a bucketlist',type: :request do
           token = token_generator(@user1)
           headers = { 'HTTP_AUTHORIZATION' => token }
           put "/bucketlists/#{@bucketlist1.id}", {
-            name: 'Alan Padew'
+            name: nil
           }, headers
         end
         it 'returns a status code of 422' do
           expect(response).to have_http_status 422
         end
         it 'return the appropriate error message to the user' do
-          expect(json['error']['name']).to eq ["can't be blank"]
+
+          expect(json['errors']['name']).to eq ["can't be blank"]
         end
       end
     end
@@ -49,14 +50,16 @@ RSpec.describe 'when trying to update a bucketlist',type: :request do
         put "/bucketlists/#{@bucketlist2.id}", {
           name: 'Alan Padew'
         }, headers
+
       end
 
       it 'returns a 404 status code' do
+
         expect(response).to have_http_status 404
       end
 
       it 'returns a message to the User' do
-        expect(json['message']).to include "can't update an invalid bucketlid"
+        expect(json['errors']).to include "can't update an invalid bucketlist"
       end
     end
   end
