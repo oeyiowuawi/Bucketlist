485e6094cd677638bd193e040b9a52610b192a8b
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-05 13:16:12 +0100

    feat(serializer): add bucketlist serializer

--- app/serializers/bucket_list_serializer.rb
+++ app/serializers/bucket_list_serializer.rb
@@ -1,3 +1,12 @@
 class BucketListSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :items, :date_created, :date_modified
+  has_many :items
+
+  def date_created
+    object.created_at.strftime("%Y-%m-%d %l:%M:%S")
+  end
+
+  def date_modified
+    object.updated_at.strftime("%Y-%m-%d %l:%M:%S")
+  end
 end
