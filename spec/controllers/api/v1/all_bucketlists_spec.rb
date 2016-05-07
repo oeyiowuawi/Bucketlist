1f76c1149a401086bce09f3e6768d55543d0f2fd
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-07 23:29:33 +0100

    test(pagination): add spec examples for pagination

--- spec/controllers/api/v1/all_bucketlists_spec.rb
+++ spec/controllers/api/v1/all_bucketlists_spec.rb
@@ -36,48 +36,125 @@ RSpec.describe "list all the bucketlists", type: :request do
     it_behaves_like "require log in before actions"
   end
 
-  context "with valid search querry" do
-    before(:all) do
+  describe "Search" do
+    before(:each) do
       create(:bucket_list, name: "Late Thirties", user: @user)
       create(:bucket_list, name: "Early Thirties", user: @user)
       create(:bucket_list, name: "Mid Twenties", user: @user)
-      headers = {
-        "HTTP_AUTHORIZATION" => @token,
-        "Content-Type" => "application/json",
-        "HTTP_ACCEPT" => "application/vnd.bucketlist.v1"
-      }
-      get "/bucketlists?q=Thirties", {}, headers
     end
-    it "returns status code of 200" do
-      expect(response).to have_http_status 200
+
+    context "with valid search querry" do
+      before(:all) do
+        headers = {
+          "HTTP_AUTHORIZATION" => @token,
+          "Content-Type" => "application/json",
+          "HTTP_ACCEPT" => "application/vnd.bucketlist.v1"
+        }
+        get "/bucketlists?q=Thirties", {}, headers
+      end
+      it "returns status code of 200" do
+        expect(response).to have_http_status 200
+      end
+      it "returns the correct number of bucketlists" do
+        expect(json.count).to eq 2
+      end
+      it "return bucketlist belonging to current user" do
+        bucket_list_name = json.map { |hsh| hsh["name"] }
+        result = bucket_list_name.all? { |name| name.include? "Thirties" }
+        expect(result).to eq true
+      end
     end
-    it "returns the correct number of bucketlists" do
-      expect(json.count).to eq 2
-    end
-    it "return bucketlist belonging to current user" do
-      bucket_list_name = json.map { |hsh| hsh["name"] }
-      result = bucket_list_name.all? { |name| name.include? "Thirties" }
-      expect(result).to eq true
+
+    context "with invalid search querry" do
+      before(:all) do
+        headers = {
+          "HTTP_AUTHORIZATION" => @token,
+          "Content-Type" => "application/json",
+          "HTTP_ACCEPT" => "application/vnd.bucketlist.v1"
+        }
+        get "/bucketlists?q=party", {}, headers
+      end
+      it "returns status code of 404" do
+        expect(response).to have_http_status 404
+      end
+      it "returns the correct number of bucketlists" do
+        expect(json["errors"]).to include "No result found"
+      end
     end
   end
 
-  context "with invalid search querry" do
+  describe "Pagination" do
     before(:all) do
-      create(:bucket_list, name: "Late Thirties", user: @user)
-      create(:bucket_list, name: "Early Thirties", user: @user)
-      create(:bucket_list, name: "Mid Twenties", user: @user)
-      headers = {
+      @bucketlist = create_list(:bucket_list, 30, created_by: @user.id)
+      @headers = {
         "HTTP_AUTHORIZATION" => @token,
         "Content-Type" => "application/json",
         "HTTP_ACCEPT" => "application/vnd.bucketlist.v1"
       }
-      get "/bucketlists?q=party", {}, headers
     end
-    it "returns status code of 404" do
-      expect(response).to have_http_status 404
+
+    context "when requesting with only page parameter" do
+      before(:each) do
+
+        get "/bucketlists?page=2", {}, @headers
+      end
+
+      it "should return just 10 bucketlists" do
+        binding.pry
+        expect(json.count).to eq 10
+      end
+      it "should return a status code of 200" do
+        expect(response).to have_http_status 200
+      end
+      it "should return the correct bucketlist" do
+        names = json.map {|hsh| hsh["name"] }
+        a = 0
+        b = 20
+        10.times do
+          expect(names[a]).to eq @bucketlist[b]
+          a += 1
+          b += 1
+        end
+      end
     end
-    it "returns the correct number of bucketlists" do
-      expect(json["errors"]).to include "No result found"
+
+    context "when requesting with only limit" do
+      before(:each) do
+        get "/bucketlists?limit=5", {}, @headers
+      end
+
+      it "returns a status code of 200" do
+        expect(response).to have_http_status 200
+      end
+
+      it "returns the number of results based on the provided limit" do
+        expect(json.count).to eq 5
+      end
+    end
+
+    context "when requesting with limit and page number" do
+      before(:each) do
+        get "/bucketlists?page=2&limit=5", {}, @headers
+      end
+
+      it "returns a status code of 200" do
+        expect(response).to have_http_status 200
+      end
+      it "returns the right number of bucketlist" do
+        expect(json.count).to eq 5
+      end
+      it "returns the right bucketlists" do
+        names = json.map {|hsh| hsh["name"] }
+        a = 0
+        b = 5
+        5.times do
+          expect(names[a]).to eq @bucketlist[b]
+          a += 1
+          b += 1
+        end
+      end
     end
   end
+
+
 end
