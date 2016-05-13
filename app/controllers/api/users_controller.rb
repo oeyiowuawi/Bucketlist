0004fe2b2b0d1d13fb4ce908674c0fff39aa0b7a
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-13 12:47:32 +0100

    feat(user model): add serializer for user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,8 @@ module Api
     def create
       user = User.new(users_params)
       if user.save
-        render json: user, status: 201, location: api_users_path(user)
+        render json: user, status: 201, location: api_users_path(user),
+               root: false
       else
         render json: { errors: user.errors }, status: 422
       end
