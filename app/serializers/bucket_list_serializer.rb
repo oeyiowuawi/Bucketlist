dc61e02daf02554183c558dd499438d2be5f0c08
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-24 14:55:01 +0100

    chore(merge conflict): resolve merge conflict

--- app/serializers/bucket_list_serializer.rb
+++ app/serializers/bucket_list_serializer.rb
@@ -1,10 +1,6 @@
 class BucketListSerializer < ActiveModel::Serializer
   include Utilities
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> master
   attributes :id, :name, :items, :date_created, :date_modified, :created_by
   has_many :items
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   include Utilities
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> master
   attributes :id, :name, :email, :date_created, :date_modified
 end
